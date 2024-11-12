9598e21139e2f9d6c65b05c09731759582c19550
euchangxian <changxian2001@gmail.com>
2024-11-13 00:07:41 +0800

    fix(blink-cmp): Add <Tab> keymap explicitly

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,16 +1,31 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+  },
   opts = {
     keymap = {
       preset = "super-tab",
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.is_in_snippet() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
       ["<C-j>"] = { "select_next", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<Up>"] = { "select_next", "fallback" },
-      ["Down"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_prev", "fallback" },
     },
     trigger = {
       completion = {
         keyword_range = "full",
+        show_in_snippet = false,
       },
       signature_help = {
         enabled = true,
---
+++ nvim/lua/plugins/lspclangd.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--enable-config",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+        },
+      },
+    },
+  },
+}
