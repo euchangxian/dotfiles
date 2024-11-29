032d032fb529c7bcd2131a3060100abfe8acd61d
euchangxian <changxian2001@gmail.com>
2024-11-29 13:47:11 +0800

    feat(nvim): Add snacks.nvim config

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    bufdelete = { enabled = true },
+    dashboard = { enabled = true },
+    git = { enabled = true },
+    notify = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
