0af6ccef34b26f5b86803f130ca740509bc8e9b4
euchangxian <changxian2001@gmail.com>
2025-09-27 12:41:29 +0800

    feat(UnionFind): Add Map-Based UF template

--- nvim/templates/UnionFind.cpp
+++ nvim/templates/UnionFind.cpp
@@ -1,12 +1,12 @@
+#include <numeric>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 class UnionFind {
  public:
   UnionFind(int n) : components_(n), parent_(n), rank_(n, 0), size_(n, 1) {
-    for (int i = 0; i < n; ++i) {
-      parent_[i] = i;
-    }
+    std::iota(parent_.begin(), parent_.end(), 0);
   }
 
   int find(int x) noexcept {
@@ -17,9 +17,9 @@ class UnionFind {
     return parent_[x];
   }
 
-  bool connected(int x, int y) { return find(x) == find(y); }
+  bool connected(int x, int y) noexcept { return find(x) == find(y); }
 
-  void unite(int x, int y) {
+  void unite(int x, int y) noexcept {
     int rootX = find(x);
     int rootY = find(y);
 
@@ -43,7 +43,7 @@ class UnionFind {
 
   int components() const noexcept { return components_; }
 
-  int size(int x) { return size_[find(x)]; }
+  int size(int x) noexcept { return size_[find(x)]; }
 
  private:
   int components_;
@@ -51,4 +51,65 @@ class UnionFind {
   std::vector<int> parent_;
   std::vector<int> rank_;
   std::vector<int> size_;
+};
+
+template <typename T, typename Hash = std::hash<T>>
+class DSU {
+ private:
+  using ConstRefT = const T&;
+
+ public:
+  DSU() : components_(0) {}
+
+  T find(ConstRefT x) noexcept {
+    if (parent_.find(x) == parent_.end()) {
+      parent_.emplace(x, x);
+      rank_.emplace(x, 0);
+      size_.emplace(x, 1);
+      ++components_;
+
+      return x;
+    }
+
+    if (parent_.at(x) != x) {
+      parent_.at(x) = find(parent_.at(x));
+    }
+    return parent_.at(x);
+  }
+
+  bool connected(ConstRefT x, ConstRefT y) noexcept {
+    return find(x) == find(y);
+  }
+
+  void unite(ConstRefT x, ConstRefT y) noexcept {
+    ConstRefT rootX = find(x);
+    ConstRefT rootY = find(x);
+    if (rootX == rootY) {
+      return;
+    }
+
+    --components_;
+    if (rank_[rootX] < rank_[rootY]) {
+      parent_[rootX] = rootY;
+      size_[rootY] += std::exchange(size_[rootX], 0);
+      return;
+    }
+
+    if (rank_[rootX] == rank_[rootY]) {
+      ++rank_[rootX];
+    }
+    parent_[rootY] = rootX;
+    size_[rootX] += std::exchange(size_[rootY], 0);
+  }
+
+  int components() const noexcept { return components_; }
+
+  int size(ConstRefT x) noexcept { return size_[find(x)]; };
+
+ private:
+  int components_;
+
+  std::unordered_map<T, T, Hash> parent_;
+  std::unordered_map<T, int, Hash> rank_;
+  std::unordered_map<T, int, Hash> size_;
 };
