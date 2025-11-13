70c15df2b148fe946dc02512f10edf0904849441
euchangxian <changxian2001@gmail.com>
2025-11-13 21:29:06 +0800

    feat(nvim): change theme to cyberdream

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -9,13 +9,27 @@ return {
   --   },
   -- },
 
+  -- {
+  --   "philosofonusus/morta.nvim",
+  --   name = "morta",
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd.colorscheme("morta")
+  --   end,
+  -- },
   {
-    "philosofonusus/morta.nvim",
-    name = "morta",
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
     priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme("morta")
+    opts = {
+      transparent = true,
+      -- italic_comments = true,
+      hide_fillchars = true,
+    },
+    config = function(_, opts)
+      require("cyberdream").setup(opts)
+      vim.cmd.colorscheme("cyberdream")
     end,
   },
 }
